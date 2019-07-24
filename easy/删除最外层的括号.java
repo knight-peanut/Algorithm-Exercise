09858153afe489c908c4b5644f269e491dc3934a
机长 <49093310+knight-peanut@users.noreply.github.com>
2019-07-24 16:19:40 +0800

    Add files via upload

---
+++ easy/删除最外层的括号.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String removeOuterParentheses(String S) {
+        StringBuilder str = new StringBuilder ();
+        Stack<Character> stack = new Stack<>(); 
+		//记录原语的开始和结束标志
+        int start = 0,end = 0;
+        boolean flag = false;
+        
+        for(int i=0;i<S.length();i++){
+			//遇到左括号，入栈
+            if(S.charAt(i)=='('){
+                stack.push(S.charAt(i));
+                if(!flag){
+					//记录下开始的位置
+                    start = i;
+                    flag = true;
+                }
+            }
+            
+            if(S.charAt(i)==')'){
+				//遇到右括号，出栈
+                stack.pop();
+                if(stack.isEmpty()){
+                    end = i;
+					//将去掉括号的原语追加到str
+                    str.append(S.substring(start + 1,end));
+                    flag = false;
+					//重新开始下一个原语的检测
+                    start = end;
+                }
+            }
+        }
+        return str.toString();
+    }
+}
+
